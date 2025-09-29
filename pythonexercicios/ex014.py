a4f7a49d9dc1e9ea001f79fe1cd1ce3bf7c8ab2e
helovidoto <mhdgheloisa@gmail.com>
2025-09-29 19:28:48 -0300

    exercícios do 009 até o 021

---
+++ pythonexercicios/ex014.py
@@ -0,0 +1,4 @@
+c = float(input('Informe a temperatura em Graus Celsius: '))
+f = ((9 * c) / 5) + 32
+print('A temperatura de {} Graus Celsius corresponde a {} Fahrenheit!'.format(c, f))
+# A conta funciona mesmo sem os parênteses por conta da ordem de precedência
